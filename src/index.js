c7544a6e15cb64d274fead26db21962f58888817
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-12-30 14:39:50 +0000

    updated sample readme

--- src/index.js
+++ src/index.js
@@ -68,20 +68,54 @@ const generateLicense = (answers) => {
 };
 
 const generateReadme = (answers) => {
-  return `${generateTitle(answers)}
+  return `# TITLE ![MIT](https://img.shields.io/static/v1?label=${}&message=License&color=green)
 
-  ${generateDescription(answers)}
+## Table of Contents
 
-  ${generateInstallation(answers)}
+  - [Table of Contents](#table-of-contents)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Contributing](#contributing)
+  - [License](#license)
 
-  ${generateUsage(answers)}
+## Description
 
-  ${generateTests(answers)}
+ADD TEXT HERE
 
-  ${generateContributing(answers)}
+## Installation
 
-  ${generateLicense(answers)}
-  `;
+Run the following script to install the packages required for the application:
+
+\`\`\`
+ADD TEXT HERE
+\`\`\`
+
+## Usage
+
+To use the application run the following script:
+
+\`\`\`
+ADD TEXT HERE
+\`\`\`
+
+## Tests
+
+To use the application run the following script:
+
+\`\`\`
+ADD TEXT HERE
+\`\`\`
+
+## Contributing
+
+ADD TEXT HERE
+
+## License
+
+ADD TEXT HERE
+`
 };
 
 const writeToFile = (filePath, data) => {
